0d771a16123610f39b05b07a016507e7890eb35f
Damon <126731021+damon314159@users.noreply.github.com>
2023-10-11 03:17:03 +0100

    Separate validity tester from .placeShip

--- src/js/Gameboard.js
+++ src/js/Gameboard.js
@@ -14,16 +14,28 @@ class Gameboard {
     this.hits = structuredClone(this.grid)
   }
 
-  placeShip(ship, r, c, direction) {
+  isValidPlacement(ship, r, c, direction) {
     // For each cell of the ship, calculate the coordinates
     for (let i = 0; i < ship.length; i += 1) {
       const rowPlacement = direction === 'vertical' ? r + i : r
       const colPlacement = direction === 'horizontal' ? c + i : c
       // If that cell is full, the ship overlaps an existing ship
       if (this.grid[rowPlacement][colPlacement]) {
-        throw new Error('Cell already occupied')
+        return false
       }
-      // Else set that cell to point to the ship
+    }
+    return true
+  }
+
+  placeShip(ship, r, c, direction) {
+    if (!this.isValidPlacement(ship, r, c, direction)) {
+      throw new Error('Invalid ship placement location attempted')
+    }
+    // For each cell of the ship, calculate the coordinates
+    for (let i = 0; i < ship.length; i += 1) {
+      const rowPlacement = direction === 'vertical' ? r + i : r
+      const colPlacement = direction === 'horizontal' ? c + i : c
+      // Set that cell to point to the ship
       this.grid[rowPlacement][colPlacement] = { ship, shipID: ship.ID }
     }
   }
